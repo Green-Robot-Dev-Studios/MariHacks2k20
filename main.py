4427a150904f1b9af0c94f53ea7650ccfa6e5ac0
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-02-29 19:35:08 +0000

    Stuff

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-#melodia main file
+import parselmouth
 
-print("Hello Melodia")
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set() # Use seaborn's default style to make attractive graphs
+
+# Plot nice figures using Python's "standard" matplotlib library
+snd = parselmouth.Sound("sing.mp3")
+plt.figure()
+plt.plot(snd.xs(), snd.values.T)
+plt.xlim([snd.xmin, snd.xmax])
+plt.xlabel("time [s]")
+plt.ylabel("amplitude")
+plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
