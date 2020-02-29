0d1ee68bb08daf94cf3ca65ff4155c3267b5dc80
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-02-29 20:53:48 +0000

    NICE

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import seaborn as sns
 #make stuff look nice 
 sns.set()
 
+'''
 def draw_spectrogram(spectrogram, dynamic_range=70):
     X, Y = spectrogram.x_grid(), spectrogram.y_grid()
     sg_db = 10 * np.log10(spectrogram.values)
@@ -14,13 +15,14 @@ def draw_spectrogram(spectrogram, dynamic_range=70):
     plt.ylim([spectrogram.ymin, spectrogram.ymax])
     plt.xlabel("time [s]")
     plt.ylabel("frequency [Hz]")
+'''
 
 def draw_pitch(pitch):
     # Extract selected pitch contour, and
     # replace unvoiced samples by NaN to not plot
     pitch_values = pitch.selected_array['frequency']
     pitch_values[pitch_values==0] = np.nan
-    plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='w')
+    plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='r')
     plt.plot(pitch.xs(), pitch_values, 'o', markersize=2)
     plt.grid(False)
     plt.ylim(0, pitch.ceiling)
@@ -29,14 +31,15 @@ def draw_pitch(pitch):
 snd = pm.Sound("audio/peak.wav")
 spectrogram = snd.to_spectrogram()
 pitch = snd.to_pitch()
+'''
 # If desired, pre-emphasize the sound fragment before calculating the spectrogram
 pre_emphasized_snd = snd.copy()
 pre_emphasized_snd.pre_emphasize()
 spectrogram = pre_emphasized_snd.to_spectrogram(window_length=0.03, maximum_frequency=8000)
+'''
 plt.figure()
-draw_spectrogram(spectrogram)
-plt.twinx()
+#draw_spectrogram(spectrogram)
+#plt.twinx()
 draw_pitch(pitch)
 plt.xlim([snd.xmin, snd.xmax])
 plt.show() 
-
