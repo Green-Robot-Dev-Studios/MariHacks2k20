f2ebf8ca049d3e80bc439e733de23db7e599dcf9
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-02-29 22:44:29 +0000

    push pull

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import numpy as np ## needed for other libs
 import fluidsynth
 import time
 
-print(f.midi2freq(f.freq2midi(480)))
+#print(f.midi2freq(f.freq2midi(480)))
 
 def find_pitch(pitch):
-  pitch_values = pitch.selected_array['frequency']
+  pitch_values = pitch.selected_array['frequency']   
   print(pitch_values)
   gen.set_tempo(0, 100)
   loop = 0
   for x in pitch_values:
-    gen.add_note(x, loop, 1, 50)
+    gen.add_note(f.midi2freq(f.freq2midi(x)), loop, 1, 50)
     loop = loop + 1
+  gen.write("export.mid")
 
 if __name__ == "__main__":
   snd = pm.Sound("audio/peak.wav")
@@ -26,6 +27,3 @@ if __name__ == "__main__":
 #g.graph(pitch)
 
 #hello my name is austin i like rubiks cube and anime girls my favourite hentai is euphoria
-
-
-
