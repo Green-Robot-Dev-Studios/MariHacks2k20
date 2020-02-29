64a2fe473da4d17a415aaabb63bd3a5332f44e25
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-02-29 21:13:21 +0000

    This is it!

--- graphing.py
+++ graphing.py
@@ -7,6 +7,32 @@ import seaborn as sns
 #make stuff look nice 
 sns.set()
 
+def draw_pitch(pitch):
+  # Extract selected pitch contour, and
+  # replace unvoiced samples by NaN to not plot
+  pitch_values = pitch.selected_array['frequency']
+  pitch_values[pitch_values==0] = np.nan
+  plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='r')
+  plt.plot(pitch.xs(), pitch_values, 'o', markersize=2)
+  plt.grid(False)
+  plt.ylim(0, pitch.ceiling)
+  plt.ylabel("fundamental frequency [Hz]")
+
+def graph(pitch):
+  snd = pm.Sound("audio/peak.wav")
+  pitch = snd.to_pitch()
+  plt.figure()
+  draw_pitch(pitch)
+  plt.xlim([snd.xmin, snd.xmax])
+  plt.show()
+
+'''
+# If desired, pre-emphasize the sound fragment before calculating the spectrogram
+pre_emphasized_snd = snd.copy()
+pre_emphasized_snd.pre_emphasize()
+spectrogram = pre_emphasized_snd.to_spectrogram(window_length=0.03, maximum_frequency=8000)
+'''
+
 '''
 def draw_spectrogram(spectrogram, dynamic_range=70):
     X, Y = spectrogram.x_grid(), spectrogram.y_grid()
@@ -16,30 +42,3 @@ def draw_spectrogram(spectrogram, dynamic_range=70):
     plt.xlabel("time [s]")
     plt.ylabel("frequency [Hz]")
 '''
-
-def draw_pitch(pitch):
-    # Extract selected pitch contour, and
-    # replace unvoiced samples by NaN to not plot
-    pitch_values = pitch.selected_array['frequency']
-    pitch_values[pitch_values==0] = np.nan
-    plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='r')
-    plt.plot(pitch.xs(), pitch_values, 'o', markersize=2)
-    plt.grid(False)
-    plt.ylim(0, pitch.ceiling)
-    plt.ylabel("fundamental frequency [Hz]")
-
-snd = pm.Sound("audio/peak.wav")
-spectrogram = snd.to_spectrogram()
-pitch = snd.to_pitch()
-'''
-# If desired, pre-emphasize the sound fragment before calculating the spectrogram
-pre_emphasized_snd = snd.copy()
-pre_emphasized_snd.pre_emphasize()
-spectrogram = pre_emphasized_snd.to_spectrogram(window_length=0.03, maximum_frequency=8000)
-'''
-plt.figure()
-#draw_spectrogram(spectrogram)
-#plt.twinx()
-draw_pitch(pitch)
-plt.xlim([snd.xmin, snd.xmax])
-plt.show() 
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import parselmouth as pm
 import graphing as g
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set()
 
 def find_pitch(pitch):
-    pitch_values = pitch.selected_array['frequency']
-    print(pitch_values)
+  pitch_values = pitch.selected_array['frequency']
+  print(pitch_values)
 
-snd = pm.Sound("audio/peak.wav")
-pitch = snd.to_pitch()
+if __name__ == "__main__":
+  snd = pm.Sound("audio/peak.wav")
+  pitch = snd.to_pitch()
 
-find_pitch(pitch)
+  find_pitch(pitch)
 
 #g.graph(pitch)
