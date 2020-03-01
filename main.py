dfa355b0645a34c5300e033288cb650713617e65
Green-Robot-Dev-Studios <nficara01@gmail.com>
2020-03-01 03:14:32 +0000

    For now

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import parselmouth as pm
-import graphing as g ## needed for graphing
+import graphing as g  ## needed for graphing
 import gen
 import sample
 import freqtomidi as f
-import numpy as np ## needed for other libs
+import numpy as np  ## needed for other libs
 import fluidsynth
 import time
 
 #print(f.midi2freq(f.freq2midi(480)))
 
-def find_pitch(pitch):
-  pitch_values = pitch.selected_array['frequency']   
-  #print(pitch_values)
-  gen.set_tempo(0, 100)
 
-  print("Before Pitch Values " + pitch_values)
+def find_pitch(pitch):
+  pitch_values = pitch.selected_array['frequency']
+  #print(pitch_values)
+  gen.set_tempo(0, 6000)
+
+  print("Before Pitch Values " + str(pitch_values))
   pitches, lengths = sample.create_sample(pitch_values)
-  print("After Pitch Change " + pitches + lengths)
+  print("After Pitch Change " + str(pitches) + str(lengths))
 
   loop = 0
+  beat = 0
   for x in pitches:
-    print("Adding note " + f.freq2midi(x) + " at time " + loop + " for " + lengths[loop])
-    gen.add_note(f.freq2midi(x), loop, lengths[loop], 50)
-    loop = loop + 1
+    if (loop < len(lengths) - 1):
+      print("Adding note " + str(f.freq2midi(x)) + " at time " + str(beat) + " for " + str(lengths[loop + 1]))
+      gen.add_note(f.freq2midi(x), beat, lengths[loop + 1], 100)
+      loop = loop + 1
+      beat = beat + lengths[loop]
   gen.write("export.mid")
 
 if __name__ == "__main__":
-  snd = pm.Sound("audio/peak.wav")
+  snd = pm.Sound("audio/24besttest.wav")
   pitch = snd.to_pitch()
 
   find_pitch(pitch)
 
-#g.graph(pitch)
+g.graph(pitch)
 
-#hello my name is austin i like rubiks cube and anime girls my favourite hentai is euphoria  
+#hello my name is austin i like rubiks cube and anime girls my favourite hentai is euphoria
--- sample.py
+++ sample.py
@@ -5,12 +5,12 @@ def create_sample(pitches):
   fixed_durations = []
   total_time = len(pitches)/bpm
   #sample 1 note for every (len(pitch_values)/bpm) pitches
-  
+
   threshold = 5
   duration = 1
   cur = -11
   for i in range(len(pitches)):
-    if pitches[i] <= cur-threshold and pitches[i] >= cur+threshold:
+    if pitches[i] <= cur+threshold and pitches[i] >= cur-threshold:
       duration = duration + 1
       continue
     else:
@@ -18,7 +18,7 @@ def create_sample(pitches):
       duration = 1
       cur = pitches[i]
       fixed_pitches.append(cur)
-  
-  
+
+
 
   return fixed_pitches, fixed_durations
